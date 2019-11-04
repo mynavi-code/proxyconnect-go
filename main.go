3f4ed7ac23d6c9693a71dd1c0bee580d6f45e13e
Hiro Suzuki <suzuki.hiroaki@mynavi.jp>
2019-11-04 14:24:33 +0000

    Refactor

--- main.go
+++ main.go
@@ -1,121 +1,76 @@
 package main
 
-// https://github.com/golang/go/issues/17227
-
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"os"
-	"io"
-	"net"
-	"net/http"
-	_ "net/http/httputil"
-	"context"
-	"time"
-	"encoding/base64"
 )
 
-// https://github.com/golang/go/issues/17227
-
 func main() {
-	proxy := os.Getenv("http_proxy")
-	u, err := url.Parse(proxy)
-	if err == nil {
-		_ = u
-		fmt.Fprintf(os.Stderr, "%s %s %s \n", u.Hostname(), u.Opaque, u.Port())
-		fmt.Fprintf(os.Stderr, "%s %s \n", u.User.String(), u.Port())
+
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage %s host port\n", os.Args[0])
+		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "%s\n", base64.StdEncoding.EncodeToString([]byte(u.User.String())))
-	fmt.Fprintf(os.Stderr, "%s\n", os.Args[1] + ":" + os.Args[2])
-	//os.Exit(255)
+	u, err := url.Parse(os.Getenv("http_proxy"))
+	if err != nil || u.Hostname() == "" || u.Port() == "" {
+		fmt.Fprintf(os.Stderr, "Error: Environment variable \"http_proxy\" is not set\n")
+		os.Exit(1)
+	}
+
+	proxyHost := u.Hostname() + ":" + u.Port()
+	destHost := os.Args[1] + ":" + os.Args[2]
+
+	fmt.Fprintf(os.Stderr, "ProxyConnect %s -> %s\n", proxyHost, destHost)
 
 	url := url.URL{
 		Scheme: "http",
-		Host:   u.Hostname() + ":" + u.Port(),
-		Opaque: os.Args[1] + ":" + os.Args[2],
+		Host:   proxyHost,
+		Opaque: destHost,
 	}
 
-	db, _ := os.Create("debug.log")
-	defer db.Close()
+	pipeRead, pipeWrite := io.Pipe()
 
-	pr, pw := io.Pipe()
-
-	req := &http.Request{
+	request := &http.Request{
 		Method: "CONNECT",
-		URL: &url,
-		Header: map[string][]string {
-		},
-		Body: pr,
-		ContentLength: -1,
+		URL:    &url,
+		Header: map[string][]string{},
+		Body:   pipeRead,
 	}
 
-	req.Header.Set("Proxy-Authorization", "Basic " + base64.StdEncoding.EncodeToString([]byte(u.User.String())))
+	request.Header.Set("Proxy-Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(u.User.String())))
 
-	var htConn net.Conn
+	response, err := http.DefaultClient.Do(request)
 
-	tr := &http.Transport{
-		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			conn, err := (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext(ctx, network, addr)
-			htConn = conn
-			return conn, err
-		},
-	}
-	client := &http.Client{
-		Transport: tr,
-	}
-
-	res, err := client.Do(req)
-	//res, err := httputil.DumpRequestOut(req, true)
-	//fmt.Printf("res: %s\nerr: %s\n", res, err)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Err %s\n", err)
+		fmt.Fprintf(os.Stderr, "HTTP Error: %s\n", err)
+		os.Exit(1)
 	}
 
-	if res.StatusCode < 200 || res.StatusCode >= 300 {
-		fmt.Fprintf(os.Stderr, "Code %s\n", 	res.StatusCode)
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		fmt.Fprintf(os.Stderr, "HTTP Error: %s %s\n", response.StatusCode, response.Status)
 		os.Exit(1)
-	} 
-
-	// res.Close = false
-
-	//conn := res.Body
-	_ = res
-	//fmt.Printf("conn: %s\n", conn)
-
+	}
 
 	go func(c io.ReadCloser) {
 		defer c.Close()
 		for true {
-			var b [40960]byte
-			n, err := c.Read(b[:])
-			fmt.Fprintf(db, "Read Result: %s %s\n", n, err)
-			db.Sync()
-			fmt.Fprintf(db, "Read: %s\n", b[:n])
-			db.Sync()
+			var b [4096]byte
+			n, _ := c.Read(b[:])
 			os.Stdout.Write(b[:n])
-			_ = err
-		}		
-	}(res.Body)
-
-	_ = pw
+		}
+	}(response.Body)
 
 	for true {
-		var b [2000]byte
+		var b [4096]byte
 		n, _ := os.Stdin.Read(b[:])
 		if n == 0 {
 			break
 		}
-		// fmt.Printf("Write: %s\n", b[:n])
-		fmt.Fprintf(db, "Write: %s\n", b[:n])
-		db.Sync()
-		// n, err := htConn.Write(b[:n])
-		n, err := pw.Write(b[:n])
-		fmt.Fprintf(db, "Write Result: %s %s\n", n, err)
-		db.Sync()
+		pipeWrite.Write(b[:n])
 	}
 }
